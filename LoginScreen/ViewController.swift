9f141770fbfff2fec13f6f8f07d79989b0ada89a
Sankara Telukutla <sankara.telukutla@gmail.com>
2015-10-13 00:06:12 -0700

    Initial files

--- LoginScreen/ViewController.swift
+++ LoginScreen/ViewController.swift
@@ -9,17 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var welcomeMessage: UITextView!
+    @IBOutlet var loginLabel: UILabel!
+    @IBOutlet var userName: UITextField!
+    @IBOutlet var password: UITextField!
+    
+    
+    @IBOutlet var submitButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func submitAction(sender: AnyObject) {
+        
+        //check if the user name if not null and matches to "neel" and password to 'neel'
+        if userName.text=="neel"&&password.text=="neel"{
+            print("credentials passed. Login successful")
+            
+            loginLabel.hidden = true
+            userName.hidden = true
+            password.hidden = true
+            submitButton.hidden = true
+            
+            welcomeMessage.hidden = false
+           
+        }
+        
+    }
+    
 }
 
